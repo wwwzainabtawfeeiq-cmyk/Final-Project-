ee3a1c66a3eaaf3cf0b797255afe34a46fe8e06a
Zainab Tawfeeiq <www.zainabtawfeeiq@gmail.com>
2026-09-15 14:24:26 +0300

    Initial project setup

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "BasraFlavor API is running"
+  });
+});
+
+module.exports = app;
